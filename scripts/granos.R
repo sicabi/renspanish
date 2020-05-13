9d096a37a5be9d589943723f7518f9a325a3b0da
Sicabi <sicabi.cruz@gmail.com>
2020-05-13 23:53:51 +0300

    Gráfico de líneas

--- scripts/granos.R
+++ scripts/granos.R
@@ -2,10 +2,12 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
-granos <- read_csv(file = "granos.csv",col_names = TRUE)
+# Barras contiguas
+granos <- read_csv(file = "input/data/CSV_files/granos.csv",col_names = TRUE)
 granos <- pivot_longer(data = granos, cols = c("Arroz","Maíz","Frijol"), 
                        names_to = c("Grano"), values_to = "Kilogramos")
-ggplot(data = granos, mapping = aes(x = Kilogramos, y = Grano)) + 
+
+granos.mex <- ggplot(data = granos, mapping = aes(x = Kilogramos, y = Grano)) + 
         geom_bar(aes(fill = País), stat = "identity", position = "dodge") +
         labs(title = "Disponibilidad anual por persona de kilogramos de granos básicos, 
              promedio 2000-2019",  
@@ -14,3 +16,7 @@ ggplot(data = granos, mapping = aes(x = Kilogramos, y = Grano)) +
              y = "Grano",
              caption = "Fuente: Elaboración con ggplot a partir de la información 
              de FAOSTAT, 2019.")
+ggsave(filename = "granos_mex.jpg", plot = granos.mex, device = "jpeg", 
+       path = "output/img/", dpi = "print", units = "cm", 
+       width = 12, height = 8)
+
